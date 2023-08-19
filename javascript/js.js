60bec09daa89fd7deb9f5b8a9f86e0f05f20a573
ntgiang01012023 <ntgiang01012023@gmail.com>
2023-08-19 15:01:18 +0700

    4

--- javascript/js.js
+++ javascript/js.js
@@ -56,4 +56,162 @@ checkboxes.forEach(checkbox => {
 });
 
 
+const offerContent = document.querySelector('.info-right-offer-content');
+const seeMoreOfferButton = document.getElementById('see-more-offer');
+let isContentExpandedOffer = false;
+
+seeMoreOfferButton.addEventListener('click', function() {
+
+  const computedStyles = window.getComputedStyle(offerContent);
+
+  if (computedStyles.maxHeight === '270px') {
+    offerContent.style.maxHeight = '600px';
+    seeMoreOfferButton.style.borderTopLeftRadius = '0';
+    seeMoreOfferButton.style.borderTopRightRadius = '0';
+    seeMoreOfferButton.style.borderBottomLeftRadius = '10px';
+    seeMoreOfferButton.style.borderBottomRightRadius = '10px';
+    seeMoreOfferButton.style.textTransform = 'uppercase';
+    seeMoreOfferButton.innerHTML = 'Thu gọn bài viết <i class="fa-solid fa-caret-up"></i>';
+    offerContent.classList.add('open');
+
+  } else {
+    offerContent.style.maxHeight = '270px';
+    seeMoreOfferButton.style.borderTopLeftRadius = '10px';
+    seeMoreOfferButton.style.borderTopRightRadius = '10px';
+    seeMoreOfferButton.style.borderBottomLeftRadius = '0';
+    seeMoreOfferButton.style.borderBottomRightRadius = '0';
+    seeMoreOfferButton.style.textTransform = 'uppercase';
+    seeMoreOfferButton.innerHTML = 'Xem thêm bài viết <i class="fa-solid fa-caret-down"></i>';
+    offerContent.classList.remove('open');
+
+  }
+});
+
+
+const offerMoreContent = document.querySelector('.info-right-offer-more-content');
+const seeMoreOfferMoreButton = document.getElementById('see-more-offer-more');
+let isContentExpandedOfferMore = false;
+
+seeMoreOfferMoreButton.addEventListener('click', function() {
+
+  const computedStyles = window.getComputedStyle(offerMoreContent);
+
+  if (computedStyles.maxHeight === '290px') {
+    offerMoreContent.style.maxHeight = '800px';
+    seeMoreOfferMoreButton.style.borderTopLeftRadius = '0';
+    seeMoreOfferMoreButton.style.borderTopRightRadius = '0';
+    seeMoreOfferMoreButton.style.borderBottomLeftRadius = '10px';
+    seeMoreOfferMoreButton.style.borderBottomRightRadius = '10px';
+    seeMoreOfferMoreButton.style.textTransform = 'uppercase';
+    seeMoreOfferMoreButton.innerHTML = 'Thu gọn bài viết <i class="fa-solid fa-caret-up"></i>';
+    offerMoreContent.classList.add('open');
+
+  } else {
+    offerMoreContent.style.maxHeight = '290px';
+    seeMoreOfferMoreButton.style.borderTopLeftRadius = '10px';
+    seeMoreOfferMoreButton.style.borderTopRightRadius = '10px';
+    seeMoreOfferMoreButton.style.borderBottomLeftRadius = '0';
+    seeMoreOfferMoreButton.style.borderBottomRightRadius = '0';
+    seeMoreOfferMoreButton.style.textTransform = 'uppercase';
+    seeMoreOfferMoreButton.innerHTML = 'Xem thêm bài viết <i class="fa-solid fa-caret-down"></i>';
+    offerMoreContent.classList.remove('open');
+
+  }
+});
+
+
+
+const productsPerPage = 3;
+let currentPage = 0;
+
+function showProducts(page) {
+    const productContainer = document.getElementById("product-container");
+    const translateX = -page * 100;
+
+    productContainer.style.transform = `translateX(${translateX}%)`;
+
+    const paginateDots = document.querySelectorAll(".paginate-dot");
+    paginateDots.forEach((dot, index) => {
+        if (index === page) {
+            dot.classList.add("active");
+        } else {
+            dot.classList.remove("active");
+        }
+    });
+}
+
+function changePage(page) {
+    currentPage = page;
+    showProducts(currentPage);
+}
+
+function showNextProducts() {
+    const productContainer = document.getElementById("product-container");
+    const products = productContainer.querySelectorAll(".accessory-product-detail");
+    
+    if (currentPage < Math.ceil(products.length / productsPerPage) - 1) {
+        currentPage++;
+        showProducts(currentPage);
+    }
+}
+
+function showPreviousProducts() {
+    if (currentPage > 0) {
+        currentPage--;
+        showProducts(currentPage);
+    }
+}
+
+showProducts(currentPage);
+
+
+
+
+const productsPerPageCompare = 2;
+let currentPageCompare = 0;
+
+function showProductsCompare(page) {
+    const productContainerCompare = document.getElementById("product-container-compare");
+    const translateX = -page * 100;
+
+    productContainerCompare.style.transform = `translateX(${translateX}%)`;
+
+    const paginateDotsCompare = document.querySelectorAll(".compare-paginate-dot");
+    paginateDotsCompare.forEach((dot, index) => {
+        if (index === page) {
+            dot.classList.add("active");
+        } else {
+            dot.classList.remove("active");
+        }
+    });
+}
+
+function changePageCompare(page) {
+    currentPageCompare = page;
+    showProductsCompare(currentPageCompare);
+}
+
+function showNextProductsCompare() {
+    const productContainerCompare = document.getElementById("product-container-compare");
+    const productsCompare = productContainerCompare.querySelectorAll(".compare-product-detail");
+    
+    if (currentPageCompare < Math.ceil(productsCompare.length / productsPerPageCompare) - 1) {
+        currentPageCompare++;
+        showProductsCompare(currentPageCompare);
+    }
+}
+
+function showPreviousProductsCompare() {
+    if (currentPageCompare > 0) {
+        currentPageCompare--;
+        showProductsCompare(currentPageCompare);
+    }
+}
+
+showProductsCompare(currentPageCompare);
+
+
+
+
+
 
